10e217055b15924cea7d7ebe6110d5716de31b27
Johan Millers <johanoscarmarte@gmail.com>
2020-11-07 17:34:56 -0400

    Destalle de la pagina de película

--- src/app/components/destalle/destalle.component.ts
+++ src/app/components/destalle/destalle.component.ts
@@ -1,3 +1,5 @@
+import { ModalController } from '@ionic/angular';
+import { Cast, PeliculaDetalle } from './../../Interfaces/interfaces';
 import { MoviesService } from './../../services/movies.service';
 import { Component, Input, OnInit } from '@angular/core';
 
@@ -9,16 +11,32 @@ import { Component, Input, OnInit } from '@angular/core';
 export class DestalleComponent implements OnInit {
   @Input() id;
 
-  constructor(private MoviesService: MoviesService) { }
+  public pelicula : PeliculaDetalle = {};
+  public Actores : Cast[] = [];
+
+  slideOptPoster = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spacebetween: -5
+  }
+
+
+
+  constructor(private MoviesService: MoviesService,
+              private ModalController: ModalController) { }
 
   ngOnInit() {
     // console.log(this.id);
     this.MoviesService.getPeliculaDestalle(this.id).subscribe(resp => {
-      console.log(resp);
+      this.pelicula = resp;
     })
     this.MoviesService.getActoresPelicula(this.id).subscribe(resp => {
-      console.log(resp.cast);
+      this.Actores = resp.cast.filter(actor => actor.profile_path !== null);
     })
+  }
+  regresar() {
+    this.ModalController.dismiss();
+
   }
 
 
