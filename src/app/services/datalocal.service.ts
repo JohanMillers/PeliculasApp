3e856b00a54968066957a28eaccb6a8f30efe4e1
Johan Millers <johanoscarmarte@gmail.com>
2020-11-10 23:03:56 -0400

    agregando pelicula a favoritos

--- src/app/services/datalocal.service.ts
+++ src/app/services/datalocal.service.ts
@@ -1,6 +1,7 @@
 import { PeliculaDetalle } from './../Interfaces/interfaces';
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,39 @@ export class DatalocalService {
   Peliculas: PeliculaDetalle[] = [];
 
 
-  constructor(private storage: Storage) { }
+  constructor(private storage: Storage,
+              private toastCtrl: ToastController) { }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 1500
+    });
+    toast.present();
+  }
 
   guardarPelicula(pelicula : PeliculaDetalle){
-    
-    this.Peliculas.push(pelicula);
+
+    let existe = false;
+    let mensaje = '';
+
+    for (const peli of this.Peliculas){
+      if (peli.id === pelicula.id){
+        existe = true;
+        break;
+      }
+    }
+
+    if (existe) {
+      this.Peliculas = this.Peliculas.filter(peli => peli.id !== pelicula.id);
+      mensaje = 'Removido de favoritos';
+    }else {
+      this.Peliculas.push(pelicula);
+      mensaje = 'Agredada a favoritos';
+    }
+
+
+    this.presentToast(mensaje);
     this.storage.set('pelicula', this.Peliculas);
   }
 
